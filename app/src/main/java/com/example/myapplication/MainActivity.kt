d77b7dfc8b07f830d591f4785c3b061fa75d5d1c
sheepyis <zx9086@naver.com>
2023-11-09 18:02:52 +0900

    🐛 충돌 수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_main)
         // setContentView(R.layout.mypage)
-        setContentView(R.layout.chatlist)
+        // setContentView(R.layout.chatlist)
     }
 }
