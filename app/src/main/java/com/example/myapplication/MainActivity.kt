d8711093894b2f9b6121f389c050d73ed34c008a
yewon0219 <123551974+yewon0219@users.noreply.github.com>
2023-11-09 16:03:13 +0900

    home 화면 구성

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.io.Console
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.home)
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_array,
+            R.layout.home
+        )
+        adapter.setDropDownViewResource(R.layout.home)
+        spinner.adapter = adapter
     }
 }
