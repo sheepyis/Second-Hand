cf6f7e2d3a5ca7152b88fe455196611b732b2a02
yujin <ohyuj2001@naver.com>
2023-11-12 12:16:54 +0900

    회원가입 성공!

--- app/src/main/java/com/example/myapplication/SignupActivity.kt
+++ app/src/main/java/com/example/myapplication/SignupActivity.kt
@@ -1,12 +1,52 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import org.w3c.dom.Text
+
+//import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        auth = FirebaseAuth.getInstance()
+
+        val signup = findViewById<Button>(R.id.signup)
+        val email = findViewById<TextView>(R.id.email)
+        val password = findViewById<TextView>(R.id.password)
+
+        signup.setOnClickListener {
+            val emailText = email.text.toString().trim()
+            val passwordText = password.text.toString().trim()
+
+            if (emailText.isEmpty() || passwordText.isEmpty()) {
+                Toast.makeText(this, "이메일과 비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                createUser(emailText, passwordText)
+            }
+        }
+
     }
+
+    private fun createUser(emailText: String, passwordText: String) {
+        auth.createUserWithEmailAndPassword(emailText, passwordText)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                    // 추가적인 작업 수행 (예: 데이터베이스에 사용자 정보 저장)
+                } else {
+                    Toast.makeText(this, "회원가입 실패: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
