fdda4e157fa7d4d6b52fb57f125021fb1eabb999
sheepyis <zx9086@naver.com>
2023-11-12 22:04:58 +0900

    ✨ 로그인 기능 완료

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,15 +1,40 @@
 package com.example.myapplication
 
-
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        // 여기에 로그인 화면의 UI 구성 및 로직을 추가하세요
+        emailEditText = findViewById(R.id.email)
+        passwordEditText = findViewById(R.id.password)
+        loginButton = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "입력한 회원 정보가 없습니다. 다시 입력해주세요", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
