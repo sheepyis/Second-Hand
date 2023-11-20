e7a8e1164c8554bf93d28de013c71f21b5d89009
sheepyis <zx9086@naver.com>
2023-11-20 17:04:00 +0900

    상품 추가 수정 완료

--- app/src/main/java/com/example/myapplication/AddActivity.kt
+++ app/src/main/java/com/example/myapplication/AddActivity.kt
@@ -10,13 +10,13 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class AddActivity : AppCompatActivity() {
+
     private lateinit var editTitle: EditText
     private lateinit var editDetail: EditText
     private lateinit var editPrice: EditText
 
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,33 +34,46 @@ class AddActivity : AppCompatActivity() {
         val addButton = findViewById<Button>(R.id.addButton)
 
         addButton.setOnClickListener {
-            val userId = firebaseAuth.currentUser?.uid
-            if (userId != null) {
-                db.collection("users").document(userId).get().addOnSuccessListener { documentSnapshot ->
-                    val nickname = documentSnapshot.getString("nickname")
+            val currentUser = firebaseAuth.currentUser
+            if (currentUser != null) {
+                val userId = currentUser.uid
 
-                    if (nickname != null) {
-                        val title = editTitle.text.toString()
-                        val detail = editDetail.text.toString()
-                        val price = editPrice.text.toString().toInt()
+                val userRef = db.collection("users").document(userId)
 
-                        val itemMap = hashMapOf(
-                            "title" to title,
-                            "detail" to detail,
-                            "price" to price
-                        )
+                userRef.get().addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val nickname = documentSnapshot.getString("nickname")
 
-                        productCollectionRef.document(nickname).set(itemMap)
-                            .addOnSuccessListener {
-                                Toast.makeText(this, "상품이 등록되었습니다.", Toast.LENGTH_SHORT).show()
-                                val intent = Intent(this, HomeActivity::class.java)
-                                startActivity(intent)
-                            }
-                            .addOnFailureListener {
-                                Toast.makeText(this, "상품 등록 실패: 모든 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
-                            }
-                    } else {
-                        Toast.makeText(this, "nickname을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+                        if (nickname != null) {
+                            val title = editTitle.text.toString()
+                            val detail = editDetail.text.toString()
+                            val priceStr = editPrice.text.toString()
+
+                            if (title.isNotEmpty() && detail.isNotEmpty() && priceStr.isNotEmpty()) {
+                                val price = priceStr.toInt()
+
+                                val itemMap = hashMapOf(
+                                    "title" to title,
+                                    "detail" to detail,
+                                    "price" to price,
+                                    "nickname" to nickname
+                                )
+
+                                productCollectionRef.add(itemMap)
+                                    .addOnSuccessListener {
+                                        Toast.makeText(this,"상품이 등록되었습니다.", Toast.LENGTH_SHORT).show()
+                                        val intent = Intent(this, HomeActivity::class.java)
+                                        startActivity(intent)
+                                    }
+                                    .addOnFailureListener {
+                                        Toast.makeText(this,"상품 등록 실패: 모든 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                                    }
+                                } else {
+                                Toast.makeText(this, "입력 필드를 모두 채워주세요.", Toast.LENGTH_SHORT).show()
+                                }
+                            } else {
+                            Toast.makeText(this, "사용자의 닉네임을 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+                        }
                     }
                 }
             } else {
@@ -69,5 +82,3 @@ class AddActivity : AppCompatActivity() {
         }
     }
 }
-
-
