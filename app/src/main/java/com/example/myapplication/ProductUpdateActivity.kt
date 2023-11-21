b402b15185fc52498f756e2b47ef49624c979474
dhkang <alceste951005@naver.com>
2023-11-21 16:35:40 +0900

    feat : 수정하기 페이지 데이터 불러오기 완료

--- app/src/main/java/com/example/myapplication/ProductUpdateActivity.kt
+++ app/src/main/java/com/example/myapplication/ProductUpdateActivity.kt
@@ -1,17 +1,54 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+
+import android.widget.Switch
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ProductUpdateActivity: AppCompatActivity(){
+
+    private val updateButton by lazy {findViewById<Button>(R.id.addButton)}
+    private val producttitle by lazy {findViewById<TextView>(R.id.textView1)} //제목
+    private val productcontent by lazy { findViewById<TextView>(R.id.textView2)} //내용
+    private val productprice by lazy {findViewById<TextView>(R.id.editTextText3)} //가격
+    private val switchcontent by lazy {findViewById<Switch>(R.id.switch2)} //스위치
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.update_product)
 
-        val id =intent.getStringExtra("id")
-        val title = intent.getStringExtra("title")
-        val price = intent.getStringExtra("price")
-        val sold = intent.getStringExtra("sold")
-        val detail = intent.getStringExtra("detail")
+        val id =intent.getStringExtra("id") //아이디 값 - 수정하기 할때 사용할꺼임
+        var sold = intent.getStringExtra("sold")
+
+        producttitle.text = intent.getStringExtra("title") //제목
+        productprice.text = intent.getStringExtra("price").toString() //가격
+        productcontent.text = intent.getStringExtra("detail") //디테일
+
+        if(sold == "true"){
+            switchcontent.text = "판매중"
+            switchcontent.isChecked = true
+        }else{
+            switchcontent.text = "판매완료"
+            switchcontent.isChecked = false
+        }
+
+        switchcontent.setOnCheckedChangeListener { _, isChecked ->
+            sold = if (isChecked) {
+                "true"
+            } else {
+                "false"
+            }
+
+            switchcontent.text = if (isChecked) {
+                "판매중"
+            } else {
+                "판매완료"
+            }
+        }
+
+
     }
 }
