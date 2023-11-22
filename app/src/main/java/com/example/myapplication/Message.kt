9870cce1583fa693d7910ea0c18ff840b696ea76
yujin <ohyuj2001@naver.com>
2023-11-22 11:13:04 +0900

    Message.kt 필드 추가

--- app/src/main/java/com/example/myapplication/Message.kt
+++ app/src/main/java/com/example/myapplication/Message.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 
 data class Message(
     val sender: String,
+    val receiver: String,
+    val product: String,
     val content: String,
     val timestamp: Long
 )
